de42e22113cbeb0289c6b28c89cfd121d4644a6b
Matt Smith <mss@datera.io>
2018-01-22 15:35:36 -0600

    Fixing multipath checking so it's actually called
    Also fixed the function so it doesn't exit prematurely and runs all
    possible tests.

--- src/validators.py
+++ src/validators.py
@@ -211,75 +211,82 @@ def check_multipath():
         if not exe_check("systemctl status multipathd | grep 'Active: active'",
                          err=False):
             ff(name, "multipathd not enabled", "8D6CC096")
+    sf(name)
 
 
 def check_multipath_conf():
     name = "Multipath Conf"
     mfile = "/etc/multipath.conf"
     if not os.path.exists(mfile):
-        ff(name, "/etc/multipath.conf file not found", "1D506D89")
+        return ff(name, "/etc/multipath.conf file not found", "1D506D89")
     with io.open(mfile, 'r') as f:
-        mconf = parse_mconf(f)
+        mconf = parse_mconf(f.read())
 
     # Check defaults section
     defaults = filter(lambda x: x[0] == 'defaults', mconf)
     if not defaults:
-        return ff(name, "defaults section not found", "1D8C438C")
+        ff(name, "Missing defaults section", "1D8C438C")
 
-    defaults = defaults[0]
-    if get_os() == "ubuntu":
-        ct = False
-        for d in defaults[1]:
-            if 'checker_timer' in d:
-                ct = True
-        if not ct:
-            ff(name, "defaults section missing 'checker_timer'", "FCFE3444")
     else:
-        ct = False
-        for d in defaults[1]:
-            if 'checker_timeout' in d:
-                ct = True
-        if not ct:
-            ff(name, "defaults section missing 'checker_timeout'", "70191A9A")
+        defaults = defaults[0]
+        if get_os() == "ubuntu":
+            ct = False
+            for d in defaults[1]:
+                if 'checker_timer' in d:
+                    ct = True
+            if not ct:
+                ff(name, "defaults section missing 'checker_timer'",
+                   "FCFE3444")
+        else:
+            ct = False
+            for d in defaults[1]:
+                if 'checker_timeout' in d:
+                    ct = True
+            if not ct:
+                ff(name, "defaults section missing 'checker_timeout'",
+                   "70191A9A")
 
     # Check devices section
     devices = filter(lambda x: x[0] == 'devices', mconf)
     if not devices:
-        return ff(name, "Missing devices section", "797A6031")
-    devices = devices[0][1]
-    dat_block = None
-    for _, device in devices:
-        ddict = {}
-        for entry in device:
-            ddict[entry[0]] = entry[1]
-        if ddict['vendor'] == 'DATERA':
-            dat_block = ddict
-    if not dat_block:
-        return ff(name, "No DATERA device section found", "99B9D136")
+        ff(name, "Missing devices section", "797A6031")
+    else:
+        devices = devices[0][1]
+        dat_block = None
+        for _, device in devices:
+            ddict = {}
+            for entry in device:
+                ddict[entry[0]] = entry[1]
+            if ddict['vendor'] == 'DATERA':
+                dat_block = ddict
+        if not dat_block:
+            return ff(name, "No DATERA device section found", "99B9D136")
 
-    if not dat_block['product'] == "IBLOCK":
-        ff(name, "Datera 'product' entry should be \"IBLOCK\"", "A9DF3F8C")
+        if not dat_block['product'] == "IBLOCK":
+            ff(name, "Datera 'product' entry should be \"IBLOCK\"", "A9DF3F8C")
 
     # Blacklist exceptions
     be = filter(lambda x: x[0] == 'blacklist_exceptions', mconf)
     if not be:
         ff(name, "Missing blacklist_exceptions section", "B8C8A19C")
-    be = be[0][1]
-    dat_block = None
-    for _, device in be:
-        bdict = {}
-        for entry in device:
-            bdict[entry[0]] = entry[1]
-        if bdict['vendor'] == 'DATERA.*':
-            dat_block = bdict
-    if not dat_block:
-        ff(name, "No Datera blacklist_exceptions section found", "09E37E51")
-    if dat_block['vendor'] != 'DATERA.*':
-        ff(name, "Datera blacklist_exceptions vendor entry malformed",
-           "9990F32F")
-    if dat_block['product'] != 'IBLOCK.*':
-        ff(name, "Datera blacklist_exceptions product entry malformed",
-           "642753A0")
+    else:
+        be = be[0][1]
+        dat_block = None
+        for _, device in be:
+            bdict = {}
+            for entry in device:
+                bdict[entry[0]] = entry[1]
+            if bdict['vendor'] == 'DATERA.*':
+                dat_block = bdict
+        if not dat_block:
+            ff(name, "No Datera blacklist_exceptions section found",
+               "09E37E51")
+        if dat_block['vendor'] != 'DATERA.*':
+            ff(name, "Datera blacklist_exceptions vendor entry malformed",
+               "9990F32F")
+        if dat_block['product'] != 'IBLOCK.*':
+            ff(name, "Datera blacklist_exceptions product entry malformed",
+               "642753A0")
     sf(name)
 
 
@@ -318,7 +325,8 @@ def client_check(config):
               check_irq,
               check_cpufreq,
               check_block_devices,
-              check_multipath]
+              check_multipath,
+              check_multipath_conf]
 
     list(map(lambda x: x(), checks))
 
