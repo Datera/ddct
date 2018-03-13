a0b00838ca5b389e5a7eb0807f64682607040864
Matt Smith <mss@datera.io>
2018-03-13 15:20:57 -0500

    Merging devel branch which revamps arguments
    * Revamped arguments
    * Support for installers
    * Version now v2.0.0
    
    Squashed commit of the following:
    
    commit 401e580afdbc3a77e67783c96435a169dccf0077
    Author: Matt Smith <mss@datera.io>
    Date:   Tue Mar 13 15:07:38 2018 -0500
    
        Tag parsing fixes
    
    commit 4b64813dde508be3e1afe85e5a53514b183d426b
    Author: Matt Smith <mss@datera.io>
    Date:   Tue Feb 27 16:32:49 2018 +0000
    
        Fixing argument parse order
    
    commit db181a2065691776778fe8bab88fb48dd6441fea
    Author: Matt Smith <mss@datera.io>
    Date:   Tue Feb 27 16:22:22 2018 +0000
    
        Installer development checkpoint
    
    commit a69e5aad7b197867fe1a60fa40cc43402a5ef86f
    Author: Matt Smith <mss@datera.io>
    Date:   Thu Feb 22 12:06:01 2018 -0600
    
        Updating readme with devel branch changes
    
    commit 24d7da0dc733ca51b7b51a3dbba2533433b1444b
    Author: Matt Smith <mss@datera.io>
    Date:   Thu Feb 22 11:27:32 2018 -0600
    
        Adding missing config return value
    
    commit 42d2062abece8f36c2459b3350dba31ab4ec8008
    Author: Matt Smith <mss@datera.io>
    Date:   Thu Feb 22 11:25:45 2018 -0600
    
        Moving out of scope flags
    
    commit a57ff644332045551cf887fb608a442ef95157f7
    Author: Matt Smith <mss@datera.io>
    Date:   Thu Feb 22 11:22:00 2018 -0600
    
        Revamp of argument parser

---
+++ src/installers.py
@@ -0,0 +1,25 @@
+from __future__ import (print_function, unicode_literals, division,
+                        absolute_import)
+
+import sys
+
+from common import install_load
+
+install_list = []
+
+
+def load_plugin_installers(plugins):
+    plugs = install_load()
+    for plugin in plugins:
+        if plugin not in plugs:
+            print("Unrecognized install plugin requested:", plugin)
+            print("Available install plugins:", ", ".join(plugins.keys()))
+            sys.exit(1)
+        install_list.extend(plugs[plugin].install)
+
+
+def run_installers(config, plugins):
+    results = []
+    load_plugin_installers(plugins)
+    for installer in install_list:
+        results.append(installer(config))
