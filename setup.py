3d7137b1517a7f0cae76aaa2b4d17743421cbc63
Matt Smith <mss@datera.io>
2018-11-07 15:57:38 -0600

    Adding setup.py and setup.cfg files

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env
+
+from setuptools import setup
+
+setup(
+    name='ddct',
+    version='0.1.0',
+    description='Datera Deployment Check Tool',
+    long_description='Install Instructions: sudo python setup.py install',
+    author='Datera Ecosystem Team',
+    author_email='support@datera.io',
+    packages=['ddct'],
+    package_dir={'': 'src'},
+    # package_data={'dfs_sdk': ['log_cfg/*.json']},
+    # include_package_data=True,
+    install_requires=[],
+    scripts=[],
+    url='https://github.com/Datera/ddct/',
+)
